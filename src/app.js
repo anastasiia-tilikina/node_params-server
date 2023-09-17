b32f457e128b38b9fbe9b62d6aba5a6969bcd399
Anastasiia Tilikina <anastasiia.tilikina@bndigital.co>
2023-09-17 19:39:08 +0300

    create Params echo server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const [path, params] = req.url.slice(1).split('?');
+
+    const searchParams = new URLSearchParams(params);
+
+    const query = Object.fromEntries(searchParams.entries());
+    const parts = path.split('/');
+
+    res.end(JSON.stringify({
+      parts, query,
+    }));
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
